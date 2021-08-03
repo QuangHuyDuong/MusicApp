328df8172933e528ea2d18191b82225b31b48d6f
QuangHuyDuong <quanghuy.duong.1310@gmail.com>
2021-08-03 15:19:10 +0700

    Done

--- music.js
+++ music.js
@@ -3,19 +3,16 @@ let musicList = [
         name : 'song0',
         artist : 'artist0',
         img : 'img0',
-        src : 'audio0'
     },
     {
         name : 'song1',
         artist : 'artist1',
         img : 'img1',
-        src : 'audio1'
     },
     {
         name : 'song2',
         artist : 'artist2',
         img : 'img2',
-        src : 'audio2'
     },
     {
         name : 'song3',
@@ -27,15 +24,17 @@ let musicList = [
 var songDetails = document.querySelector(".song")
 var songName = songDetails.querySelector('.song-name')
 var artist = songDetails.querySelector('.artist')
+var image = document.querySelector(".img")
 var currentSong = document.querySelector('.my-audio')
 var currentIndex = 0;
 var isPlayed = false;
 var progressBar = document.querySelector('.progress-bar')
 
 var playBtn = document.querySelector(".play-pause")
-console.log(playBtn)
+var isReplayed = false
+var moreBtn = document.querySelector(".fa-list-ul")
 var myAudio= document.querySelector(".my-audio"); 
-
+var listSong = document.querySelector(".music-list")
 
 //load songs
 
@@ -44,41 +43,51 @@ function loadSong(index){
     
     songName.innerHTML = musicList[index].name
     artist.innerHTML = musicList[index].artist
-    currentSong.setAttribute("src",`asset/music/1/song${currentIndex}.mp3`)
+    currentSong.setAttribute("src",`asset/music/1/song${index}.mp3`)
     myAudio.addEventListener('loadeddata',() => {
         var minute = Math.floor(myAudio.duration/60)
         var second = Math.floor( myAudio.duration%60)
         document.querySelector('.end').innerHTML = `${minute} : ${second}`
     })
+    image.innerHTML = `<img class="img-width" src="asset/music/1/img${index}.jpg" alt="">`
+    progressBar.style.width = 0;
+    if(isPlayed) myAudio.play()
 }
 // load song at first
 window.addEventListener('load',() =>{
     document.querySelector(".play-pause").innerHTML = `<i onclick="playAudio()" class="fas fa-play"></i>`
     progressBar.style.width = '0%'
-    
     loadSong(0)
+    //Load list song when at start
+    var html =''
+    var i = 0
+    musicList.forEach(element => {
+        html += `<li onclick="chooseSong(${i})" ><h4>${element.name}</h4><p>${element.artist}</p></li> `
+        i++;
+    });
+    document.querySelector(".music-list ul").innerHTML = html
   
   
 })
-
-
-
-
-
+//Load next song when current song is played done or replay current song when replay button is clicked
+myAudio.addEventListener("timeupdate", () => {
+    if(myAudio.currentTime == myAudio.duration && !isReplayed){
+      nextSong()
+    }
+    if(myAudio.currentTime == myAudio.duration && isReplayed) {
+        myAudio.currentTime =0
+        myAudio.play()
+    }
+})
 // play or pause the music
-
-
 function playAudio() { 
   isPlayed = true;
   
   myAudio.play(); 
   playBtn.innerHTML = ` <i class="fas fa-pause"></i>`
   document.querySelector(".play-pause").removeAttribute('onclick')
-  document.querySelector(".play-pause").setAttribute('onclick','pauseAudio()')
-  
-  
+  document.querySelector(".play-pause").setAttribute('onclick','pauseAudio()') 
 } 
-
 function pauseAudio() { 
   isPlayed = false
   myAudio.pause(); 
@@ -136,6 +145,37 @@ progress.onclick = function(e){
   myAudio.currentTime = (clickPoint/totalBar) * myAudio.duration
 }
 
+//Loop the song
 
+function loopSong(){
+    let replayBtn = document.querySelector(".fa-retweet")
+    console.log(replayBtn)
+    if(!isReplayed){
+        isReplayed = true;
+        replayBtn.style.color = 'pink'
+    } else {
+        isReplayed = false;
+        replayBtn.style.color = 'black'
+    }
+}
 
+// Show/Hide song list
+moreBtn.onclick = () => {
+    listSong.style.bottom = '0'
+    listSong.style.opacity = '1'
+    document.querySelector(".music-list").style.pointerEvents = 'initial'
+}
+document.querySelector(".fa-times").onclick =  () => {
+    listSong.style.bottom = '-55%'
+    listSong.style.opacity ='0'
+    document.querySelector(".music-list").style.pointerEvents = 'none'
 
+}
+// Choose song from the list add close the list
+function chooseSong(index){
+    loadSong(index)
+    listSong.style.bottom = '-55%'
+    listSong.style.opacity ='0'
+    document.querySelector(".music-list").style.pointerEvents = 'none'
+
+}
